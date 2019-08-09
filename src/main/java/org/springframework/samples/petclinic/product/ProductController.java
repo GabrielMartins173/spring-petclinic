5f283fd4efcbb0a4add74e3e9d1edee27bc27a82
Gabriel Martins <gabrielmartins173@gmail.com>
2019-08-09 17:02:34 -0300

    #3 Added a new buttom called Products that contains the list of products.

--- src/main/java/org/springframework/samples/petclinic/product/ProductController.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductController.java
@@ -1,15 +1,23 @@
 package org.springframework.samples.petclinic.product;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class ProductController {
 
     @GetMapping("/products")
-    public String showProductList(){
-        return "Hello!";
+    public String showProductList(Model model){
+        List<Product> prods = new ArrayList<>();
+        model.addAttribute("products", prods);
+        prods.add(new Product("Produto 1"));
+        prods.add(new Product("Produto 2"));
+        return "products/productsList";
     }
 
 
